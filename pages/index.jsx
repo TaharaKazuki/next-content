8f849187e0f78876fb42256276acf9efa10bec46
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-21 01:19:21 +0900

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import ResourceHighlight from '../components/ResourceHighlight'
-import Newsletter from '../components/Newsletter'
-import ResorceList from '../components/ResourceList'
-import Footer from '../components/Footer'
+import Navbar from 'components/Navbar'
+import ResourceHighlight from 'components/ResourceHighlight'
+import Newsletter from 'components/Newsletter'
+import ResorceList from 'components/ResourceList'
+import Footer from 'components/Footer'
 
 const Home = () => {
   return (
