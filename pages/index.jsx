8cdc338376ba98b941a356200db959985af9dd64
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-21 21:24:30 +0900

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,12 +7,15 @@ import Newsletter from 'components/Newsletter'
 import ResorceList from 'components/ResourceList'
 import Footer from 'components/Footer'
 
+import { resources } from 'api/data'
+
 const Home = () => {
   return (
     <Layout>
       <ResourceHighlight />
       <Newsletter />
       <ResorceList />
+      {JSON.stringify(resources)}
       <Footer />
     </Layout>
   )
