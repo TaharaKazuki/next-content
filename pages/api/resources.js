f011d290b0eafadc60ce4a3b1b36684544a6eb55
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-25 00:27:33 +0900

    update

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,4 +1,4 @@
-const resources = async (_req, res) => {
+const resources = async (req, res) => {
   if (req.method === 'GET') {
     const dataRes = await fetch('http://localhost:3001/api/resources')
     const data = await dataRes.json()
