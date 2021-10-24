0429fd2b63814cf75625430309bc1e39eb9d01e8
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-24 19:38:31 +0900

    update

--- pages/resources/new.jsx
+++ pages/resources/new.jsx
@@ -11,13 +11,62 @@ const ResourceCreate = () => {
               <h1 className="title">Add New Resource</h1>
               <form>
                 <div className="field">
-                  <label className="label">Name</label>
+                  <label className="label">Title</label>
                   <div className="control">
                     <input
                       className="input"
                       type="text"
-                      placeholder="Text input"
+                      placeholder="Learn Next JS an Sanity io"
                     />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Description</label>
+                  <div className="control">
+                    <textarea
+                      className="textarea"
+                      placeholder="Learn these tech"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Link</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="https://academy.eincode.com"
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Priority</label>
+                  <div className="control">
+                    <div className="select">
+                      <select>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Time to finish</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="number"
+                      placeholder="60 (time is in minutes)"
+                    />
+                  </div>
+                </div>
+                <div className="field is-grouped">
+                  <div className="control">
+                    <button className="button is-link">Submit</button>
+                  </div>
+                  <div className="control">
+                    <button className="button is-link is-light">Cancel</button>
                   </div>
                 </div>
               </form>
