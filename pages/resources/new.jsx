c1361f95f49be9c269bdb83f28ead188c6eb1b93
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-24 19:28:13 +0900

    update

--- pages/resources/new.jsx
+++ pages/resources/new.jsx
@@ -4,7 +4,27 @@ import Layout from 'components/Layout'
 const ResourceCreate = () => {
   return (
     <Layout>
-      <div>I am resource create page</div>
+      <div className="container">
+        <div className="colums">
+          <div className="colum is-8 is-offset-2">
+            <div className="resource-form">
+              <h1 className="title">Add New Resource</h1>
+              <form>
+                <div className="field">
+                  <label className="label">Name</label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Text input"
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
