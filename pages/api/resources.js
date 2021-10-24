596eee99718e91de14c8576cc64358b856cee568
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-25 00:29:45 +0900

    update

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -6,6 +6,10 @@ const resources = async (req, res) => {
   }
 
   if (req.method === 'POST') {
+    const { title, description, link, timeToFinish } = req.body
+    if (!title || !description || !link || !timeToFinish) {
+      return res.status(422).send('Data missing')
+    }
     return res.send('DATA has been received')
   }
 }
