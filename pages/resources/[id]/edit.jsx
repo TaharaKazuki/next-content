0aa3ce02ee623fb081fa13accf2799d8c7100f85
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-27 01:11:24 +0900

    update

--- pages/resources/[id]/edit.jsx
+++ pages/resources/[id]/edit.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import Layout from 'components/Layout'
+import ResourceForm from 'components/ResourceForm'
 
-const ResourceEdit = () => {
-  return <Layout>I am edit page</Layout>
+const ResourceEdit = ({ resource }) => {
+  const updateResorce = (formData) => {
+    alert(JSON.stringify(formData))
+  }
+
+  return (
+    <Layout>
+      <div className="container">
+        <div className="colums">
+          <div className="colum is-8 is-offset-2">
+            <ResourceForm initialData={resource} onFormSubmit={updateResorce} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const dataRes = await fetch(
+    `http://localhost:3001/api/resources/${params.id}`
+  )
+  const data = await dataRes.json()
+
+  return {
+    props: {
+      resource: data
+    }
+  }
 }
 
 export default ResourceEdit
