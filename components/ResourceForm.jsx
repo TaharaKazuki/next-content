8614dba1196fa015f9b6a6a90eb74fc84f7ca8e6
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-27 00:25:39 +0900

    update

---
+++ components/ResourceForm.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from 'react'
+
+const DEFAULT_DATA = {
+  title: '',
+  description: '',
+  link: '',
+  priority: '2',
+  timeToFinish: 60
+}
+
+const ResourceForm = ({ onFormSubmit }) => {
+  const [form, setForm] = useState(DEFAULT_DATA)
+
+  const resetForm = () => setForm(DEFAULT_DATA)
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setForm({
+      ...form,
+      [name]: value
+    })
+  }
+
+  const submitForm = () => {
+    onFormSubmit(form)
+  }
+
+  return (
+    <div className="resource-form">
+      <h1 className="title">Add New Resource</h1>
+      <form>
+        <div className="field">
+          <label className="label">Title</label>
+          <div className="control">
+            <input
+              value={form.title}
+              name="title"
+              onChange={handleChange}
+              className="input"
+              type="text"
+              placeholder="Learn Next JS an Sanity io"
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Description</label>
+          <div className="control">
+            <textarea
+              value={form.description}
+              name="description"
+              onChange={handleChange}
+              className="textarea"
+              placeholder="Learn these tech"
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Link</label>
+          <div className="control">
+            <input
+              value={form.link}
+              name="link"
+              onChange={handleChange}
+              className="input"
+              type="text"
+              placeholder="https://academy.eincode.com"
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Priority</label>
+          <div className="control">
+            <div className="select">
+              <select
+                value={form.priority}
+                name="priority"
+                onChange={handleChange}
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Time to finish</label>
+          <div className="control">
+            <input
+              value={form.timeToFinish}
+              name="timeToFinish"
+              onChange={handleChange}
+              className="input"
+              type="number"
+              placeholder="60 (time is in minutes)"
+            />
+            <p className="help">Time is in minutes</p>
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              type="button"
+              className="button is-link"
+              onClick={submitForm}
+            >
+              Submit
+            </button>
+          </div>
+          <div className="control">
+            <button
+              type="button"
+              className="button is-link is-light"
+              onClick={resetForm}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default ResourceForm
--- pages/resources/new.jsx
+++ pages/resources/new.jsx
@@ -1,136 +1,25 @@
-import React, { useState } from 'react'
-import { useRoute, useRouter } from 'next/router'
+import React from 'react'
 import Layout from 'components/Layout'
 import axios from 'axios'
-
-const DEFAULT_DATA = {
-  title: '',
-  description: '',
-  link: '',
-  priority: '2',
-  timeToFinish: 60
-}
+import { useRouter } from 'next/router'
+import ResourceForm from 'components/ResourceForm'
 
 const ResourceCreate = () => {
-  const [form, setForm] = useState(DEFAULT_DATA)
   const router = useRouter()
-  const submitForm = () => {
+
+  const createResource = (formData) => {
     axios
-      .post('/api/resources', form)
+      .post('/api/resources', formData)
       .then((_) => router.push('/'))
       .catch((err) => alert(err?.response?.data))
   }
-
-  const handleTitleChange = (e) => {
-    console.info('name', e.target.name)
-    console.info('value', e.target.value)
-    const { name, value } = e.target
-    setForm({
-      ...form,
-      [name]: value
-    })
-  }
-
-  const resetForm = () => setForm(DEFAULT_DATA)
 
   return (
     <Layout>
       <div className="container">
         <div className="colums">
           <div className="colum is-8 is-offset-2">
-            <div className="resource-form">
-              <h1 className="title">Add New Resource</h1>
-              <form>
-                <div className="field">
-                  <label className="label">Title</label>
-                  <div className="control">
-                    <input
-                      value={form.title}
-                      name="title"
-                      onChange={handleTitleChange}
-                      className="input"
-                      type="text"
-                      placeholder="Learn Next JS an Sanity io"
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Description</label>
-                  <div className="control">
-                    <textarea
-                      value={form.description}
-                      name="description"
-                      onChange={handleTitleChange}
-                      className="textarea"
-                      placeholder="Learn these tech"
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Link</label>
-                  <div className="control">
-                    <input
-                      value={form.link}
-                      name="link"
-                      onChange={handleTitleChange}
-                      className="input"
-                      type="text"
-                      placeholder="https://academy.eincode.com"
-                    />
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Priority</label>
-                  <div className="control">
-                    <div className="select">
-                      <select
-                        value={form.priority}
-                        name="priority"
-                        onChange={handleTitleChange}
-                      >
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                      </select>
-                    </div>
-                  </div>
-                </div>
-                <div className="field">
-                  <label className="label">Time to finish</label>
-                  <div className="control">
-                    <input
-                      value={form.timeToFinish}
-                      name="timeToFinish"
-                      onChange={handleTitleChange}
-                      className="input"
-                      type="number"
-                      placeholder="60 (time is in minutes)"
-                    />
-                    <p className="help">Time is in minutes</p>
-                  </div>
-                </div>
-                <div className="field is-grouped">
-                  <div className="control">
-                    <button
-                      type="button"
-                      className="button is-link"
-                      onClick={submitForm}
-                    >
-                      Submit
-                    </button>
-                  </div>
-                  <div className="control">
-                    <button
-                      type="button"
-                      className="button is-link is-light"
-                      onClick={resetForm}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
+            <ResourceForm onFormSubmit={createResource} />
           </div>
         </div>
       </div>
