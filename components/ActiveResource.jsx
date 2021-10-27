bc326f5d71d06e20d83398cf1d945173b51c8545
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-27 21:59:10 +0900

    update

--- components/ActiveResource.jsx
+++ components/ActiveResource.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
+import axios from 'axios'
 
 const ActiveResource = () => {
+  const [resource, setResource] = useState({})
+
+  useEffect(() => {
+    const fetchResouce = async () => {
+      const axiosRes = await axios.get('/api/activeResource')
+      const resource = axiosRes.data
+      setResource(resource)
+    }
+
+    fetchResouce()
+  }, [])
+
   return (
     <div className="active-resource">
-      <h1 className="resource-name">My Active</h1>
+      <h1 className="resource-name">{resource.title}</h1>
       <div className="time-wrapper">
         <h2 className="elapsed-time">1400</h2>
       </div>
