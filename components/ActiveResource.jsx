c1291c8deefa7bbd5d84c0ad9e901df77258f960
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-29 00:25:26 +0900

    update

--- components/ActiveResource.jsx
+++ components/ActiveResource.jsx
@@ -5,6 +5,7 @@ import moment from 'moment'
 
 const ActiveResource = () => {
   const [resource, setResource] = useState({})
+  const [seconds, setSeconds] = useState()
 
   useEffect(() => {
     const fetchResouce = async () => {
@@ -15,10 +16,11 @@ const ActiveResource = () => {
         moment(resource.activationTime),
         'seconds'
       )
-      const updateTimeToFinish = timeToFinish * 60 - elapsedTime
+      const updatedTimeToFinish = timeToFinish * 60 - elapsedTime
 
-      if (updateTimeToFinish >= 0) {
-        resource.timeToFinish = updateTimeToFinish
+      if (updatedTimeToFinish >= 0) {
+        resource.timeToFinish = updatedTimeToFinish
+        setSeconds(updatedTimeToFinish)
       }
       setResource(resource)
     }
@@ -26,11 +28,23 @@ const ActiveResource = () => {
     fetchResouce()
   }, [])
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds - 1)
+    }, 1000)
+
+    if (seconds < 0) {
+      clearInterval(interval)
+    }
+
+    return () => clearInterval(interval)
+  }, [seconds])
+
   return (
     <div className="active-resource">
       <h1 className="resource-name">{resource.title}</h1>
       <div className="time-wrapper">
-        <h2 className="elapsed-time">{resource.timeToFinish}</h2>
+        <h2 className="elapsed-time">{seconds}</h2>
       </div>
       <Link href="/">
         <a className="button">Go to resource</a>
