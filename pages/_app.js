ada234ec021720390805dd85ee826895b727ae4c
Tahara Kazuki <>
2022-08-27 16:47:56 +0900

    refresh

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import 'bulma/css/bulma.min.css'
-
-function MainApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MainApp
--- pages/api/activeResource.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export default async function activeResource(req, res) {
-  const axiosRes = await axios.get('http://localhost:3001/api/activeResource')
-  const resource = axiosRes.data
-  return res.send(resource)
-}
