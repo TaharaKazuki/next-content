ec5f216e60f53a3b38e33bae1cc9169881b47d04
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-08-28 23:39:47 +0900

    update

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -9,20 +9,27 @@ type User = {
 }
 
 const CreateAccount = () => {
-  const { register, handleSubmit } = useForm<User>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<User>()
 
   const submit = (data: User) => {
     console.info(data)
   }
 
   return (
-    <div>
+    <div className="container">
       <h1>アカウント作成</h1>
 
-      <form onSubmit={handleSubmit(submit)}>
+      <form onSubmit={handleSubmit(submit)} className="space-y-6">
         <div>
-          <label htmlFor="name">名前</label>
+          <label className="block mb-0.5" htmlFor="name">
+            名前
+          </label>
           <input
+            className="rounded border border-slate-300"
             {...register('name', {
               required: '必須入力です',
               maxLength: {
@@ -34,28 +41,40 @@ const CreateAccount = () => {
             name="name"
             type="text"
           />
+          {errors.name && <p className="text-red-500 mt-0.5">{errors.name.message}</p>}
         </div>
 
         <div>
-          <label htmlFor="nickName">ニックネーム</label>
+          <label className="block mb-0.5" htmlFor="nickName">
+            ニックネーム
+          </label>
           <input
-            {...register('nickName', { required: '必須入力です' })}
+            className="rounded border border-slate-300"
+            {...register('nickName', {
+              required: '必須入力です',
+              maxLength: { value: 50, message: '最大50文字です' },
+            })}
             id="nickName"
             name="nickName"
             type="text"
           />
+          {errors.nickName && <p className="text-red-500 mt-0.5">{errors.nickName.message}</p>}
         </div>
 
         <div>
-          <label htmlFor="profile">プロフィール</label>
+          <label className="block mb-0.5" htmlFor="profile">
+            プロフィール
+          </label>
           <textarea
+            className="rounded border border-slate-300"
             {...register('profile', {
               required: '必須入力です',
-              maxLength: { value: 255, message: '最大100文字です' },
+              maxLength: { value: 255, message: '最大255文字です' },
             })}
             id="profile"
             name="profile"
           />
+          {errors.profile && <p className="text-red-500 mt-0.5">{errors.profile.message}</p>}
         </div>
 
         <Button>アカウント作成</Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        md: '24px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
