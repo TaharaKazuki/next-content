6bbf618e5b649694dfbb5977ee5d8e2ca601ac76
Tahara Kazuki <>
2022-08-28 10:19:45 +0900

    update

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,19 @@
 import { Button } from '@/components/button'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { auth } from '../firebase/client'
 
 const LoginPage = () => {
+  const login = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth, provider).then((result) => {
+      alert(`こんにちは${result.user.displayName}`)
+    })
+  }
+
   return (
     <div>
       <h1>ログイン</h1>
-      <Button>ログインする</Button>
+      <Button onClick={login}>ログインする</Button>
     </div>
   )
 }
