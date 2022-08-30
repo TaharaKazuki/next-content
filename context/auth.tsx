2e8a2f8b0ff6912ee6a0f34f23535cd47d6c36e9
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-08-30 23:43:39 +0900

    update

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,10 +1,13 @@
-import { onAuthStateChanged, User } from 'firebase/auth'
-import { auth } from '../firebase/client'
+import { onAuthStateChanged, User as FirebaseUser } from 'firebase/auth'
+import { auth, db } from '../firebase/client'
 import { createContext, ReactNode, FC, useEffect, useState, useContext } from 'react'
+import { User } from 'types/user'
+import { doc, onSnapshot } from 'firebase/firestore'
 
 type ContextType = {
-  fbUser: User | null | undefined
+  fbUser: FirebaseUser | null | undefined
   isLoading: boolean
+  user: User | undefined | null
 }
 
 type ProviderProps = {
@@ -14,20 +17,30 @@ type ProviderProps = {
 const AuthContext = createContext<ContextType>({
   fbUser: undefined,
   isLoading: true,
+  user: undefined,
 })
 
 export const AuthProvider: FC<ProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>()
   const [isLoading, setIsLoading] = useState(true)
-  const [fbUser, setFbUser] = useState<User | null>()
+  const [fbUser, setFbUser] = useState<FirebaseUser | null>()
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setFbUser(user)
+    onAuthStateChanged(auth, (resultUser) => {
+      setFbUser(resultUser)
       setIsLoading(false)
+      if (resultUser) {
+        const ref = doc(db, `users/${resultUser.uid}`)
+        onSnapshot(ref, (snap) => {
+          setUser(snap.data() as User)
+        })
+      } else {
+        setUser(null)
+      }
     })
   }, [])
 
-  return <AuthContext.Provider value={{ isLoading, fbUser }}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={{ isLoading, fbUser, user }}>{children}</AuthContext.Provider>
 }
 
 export const useAuth = () => useContext(AuthContext)
